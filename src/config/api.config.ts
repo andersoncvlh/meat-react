cae0f4642e970bd71f46fc248abd7a57b2ab05a0
Anderson Carvalho Martins <CIANDT\andersoncm@lnb028002cps.ciandt.global>
2021-01-11 16:28:58 -0300

    add axios config

---
+++ src/config/api.config.ts
@@ -0,0 +1,12 @@
+import Axios from "axios";
+
+const api = Axios.create({
+  baseURL: process.env.REACT_APP_BASE_API_SIMULATOR,
+  headers: {
+    Accept: "Application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  },
+});
+
+export default api;
